058e28a33e1483cf84f7b212202aa1e2922bc805
evelyn <evelyn.fernandes4@gmail.com>
2024-09-04 13:18:38 -0300

    Fix: adding the structure of the parent comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = 'Joaquim';
+
+  userData = {
+    email: 'joaquin@email.com',
+    role: 'Admin',
+  };
+
   title = 'curso-angular';
 }
--- src/app/components/parent-data/parent-data.component.ts
+++ src/app/components/parent-data/parent-data.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-parent-data',
   templateUrl: './parent-data.component.html',
-  styleUrls: ['./parent-data.component.css']
+  styleUrls: ['./parent-data.component.css'],
 })
 export class ParentDataComponent implements OnInit {
+  @Input() name: string = '';
+  @Input() userData!: { email: string; role: string }; // using ! to say that this data is gonna be initialized later
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
