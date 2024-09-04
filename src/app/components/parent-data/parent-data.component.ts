5c4f26eaf6ce9c1ce92f16334205510ac27ff0f6
evelyn <evelyn.fernandes4@gmail.com>
2024-09-04 13:07:31 -0300

    Create: new component

---
+++ src/app/components/parent-data/parent-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-data',
+  templateUrl: './parent-data.component.html',
+  styleUrls: ['./parent-data.component.css']
+})
+export class ParentDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
